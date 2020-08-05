7fc813a335856a2fd580c26b93441728431f2f05
Rafael Holanda <rafaelholanda3105@gmail.com>
2020-08-05 14:13:21 -0300

    Added main content to TeacherList page

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
 
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
+
 import './styles.css';
 
 function TeacherList() {
@@ -25,6 +27,35 @@ function TeacherList() {
           </div>
         </form>
       </PageHeader>
+
+      <main>
+        <article className="teacher-item">
+          <header>
+            <img src="https://avatars3.githubusercontent.com/u/30013234?s=460&v=4" alt="Rafael Holanda"/>
+            <div>
+              <strong>Rafael Holanda</strong>
+              <span>Química</span>
+            </div>
+          </header>
+
+          <p>
+           Sou engenheiro de produção de formação. 
+           <br /><br />
+           Atualmente trabalho como técnico de operação off-shore na Petrobras. Sou aficionado por programação e tecnologia e pretendo me especializar e me desenvolver nessa área.
+          </p>
+
+          <footer>
+            <p>
+              Preço/Hora
+              <strong>R$ 30,00</strong>
+            </p>
+            <button type="button">
+              <img src={whatsappIcon} alt="WhatsApp"/>
+              Entrar em contato
+            </button>
+          </footer>
+        </article>
+      </main>
     </div>
   )
 }
