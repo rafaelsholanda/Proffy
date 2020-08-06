c28905d251e2320f9b5529355ea9ca3d00251d35
Rafael Holanda <rafaelholanda3105@gmail.com>
2020-08-06 05:10:39 -0300

    created database connection

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,4 +2,10 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
+app.get("/", (request, response) => {
+  return response.json("Hello World");
+});
+
 app.listen(3333);
