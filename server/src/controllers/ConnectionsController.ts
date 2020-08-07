fe438385b1226f8b85a5269df0a834016ef65794
Rafael Holanda <rafaelholanda3105@gmail.com>
2020-08-07 10:24:03 -0300

    Edited Connections Controller

--- server/src/controllers/ConnectionsController.ts
+++ server/src/controllers/ConnectionsController.ts
@@ -4,7 +4,11 @@ import db from '../database/connections';
 export default class ConnectionsController{
   
   async index(request: Request, response: Response){
+    const totalConnections = await db('connections').count('* as total');
 
+    const {total}  = totalConnections[0];
+
+    return response.json({total});
   }
   
   async create(request: Request, response: Response){
