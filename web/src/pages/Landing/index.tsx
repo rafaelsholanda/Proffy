ed086209c3009a6f08428b729cab416325ed5c09
Rafael Holanda <rafaelholanda3105@gmail.com>
2020-08-08 17:37:37 -0300

    created api.ts file with axios

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -11,39 +11,41 @@ import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 import './styles.css'; 
 
 function Landing() {
-    return (
-        <div id="page-landing">
-            <div id="page-landing-content" className="container">
-                <div className="logo-container">
-                    <img src={logoImg} alt="Proffy"/>
-                    <h2>Sua plataforma de estudos online.</h2>
-                </div>
-
-                <img 
-                    src={landingImg} 
-                    alt="Plataforma de Estudos" 
-                    className="hero-image"
-                />
-
-                <div className="buttons-container">
-                    <Link to="/study" className="study">
-                        <img src={studyIcon} alt="Estudar"/>
-                        Estudar
-                    </Link>
-
-                    <Link to="/give-classes" className="give-classes">
-                        <img src={giveClassesIcon} alt="Dar aulas"/>
-                        Dar aulas
-                    </Link>
-                </div>
-
-                <span className="total-connections">
-                    Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo"/>
-                </span>
-
-            </div>
+  
+  
+  return (
+    <div id="page-landing">
+      <div id="page-landing-content" className="container">
+        <div className="logo-container">
+          <img src={logoImg} alt="Proffy"/>
+            <h2>Sua plataforma de estudos online.</h2>
         </div>
-    )
+
+        <img 
+          src={landingImg} 
+          alt="Plataforma de Estudos" 
+          className="hero-image"
+        />
+
+        <div className="buttons-container">
+          <Link to="/study" className="study">
+            <img src={studyIcon} alt="Estudar"/>
+            Estudar
+          </Link>
+
+          <Link to="/give-classes" className="give-classes">
+            <img src={giveClassesIcon} alt="Dar aulas"/>
+            Dar aulas
+          </Link>
+        </div>
+
+        <span className="total-connections">
+          Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo"/>
+        </span>
+
+      </div>
+    </div>
+  )
 }
 
 export default Landing;
