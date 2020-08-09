508629fdce668c8dc11bddc864a4c1bd9979af4c
Rafael Holanda <rafaelholanda3105@gmail.com>
2020-08-09 04:43:12 -0300

    Edited TeacherList

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -1,35 +1,57 @@
 import React from 'react';
+import api from '../../services/api';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
 import './styles.css';
 
-function TeacherItem() {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemsProps{
+  teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemsProps> = ({teacher}) => {
+ 
+  function createNewConnection(){
+    api.post('/connectios', {
+      user_id: teacher.id,
+    });
+  }
+
   return (
     <article className="teacher-item">
       <header>
-        <img src="https://avatars3.githubusercontent.com/u/30013234?s=460&v=4" alt="Rafael Holanda"/>
+        <img src={teacher.avatar} alt={teacher.name}/>
         <div>
-          <strong>Rafael Holanda</strong>
-          <span>Química</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
-      <p>
-        Sou engenheiro de produção de formação. 
-        <br /><br />
-        Atualmente trabalho como técnico de operação off-shore na Petrobras. Sou aficionado por programação e tecnologia e pretendo me especializar e me desenvolver nessa área.
-      </p>
+      <p>{teacher.bio}</p>
 
       <footer>
         <p>
           Preço/Hora
-          <strong>R$ 30,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a 
+          target="_blank" 
+          onClick={createNewConnection} 
+          href={`https://wa.me/${teacher.whatsapp}`}
+        >
           <img src={whatsappIcon} alt="WhatsApp"/>
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
