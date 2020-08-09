e26f02a272ab05173777756c878f48a2e9abaa2f
Rafael Holanda <rafaelholanda3105@gmail.com>
2020-08-09 14:05:22 -0300

    Edited StudyTabs

--- mobile/src/components/PageHeader/index.tsx
+++ mobile/src/components/PageHeader/index.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
-import { View, Image } from 'react-native';
+import { View, Image, Text } from 'react-native';
 import { BorderlessButton } from 'react-native-gesture-handler';
 
 import backIcon from '../../assets/images/icons/back.png';
 import logoImg from '../../assets/images/logo.png';
 
 import styles from './styles';
+import { useNavigation } from '@react-navigation/native';
 
-function PageHeader(){
+interface PageHeaderProps {
+  title: string;
+}
+
+const PageHeader: React.FC<PageHeaderProps> = ({ title }) => {
+  const { navigate } = useNavigation();
+  
   function handlerGoBack() {
-
+    navigate('Landing');
   }
 
   return (
@@ -21,6 +28,8 @@ function PageHeader(){
 
         <Image source={logoImg} resizeMode='contain'/>
       </View>
+
+      <Text style={styles.title}>{title}</Text>
     </View>
   );
 }
